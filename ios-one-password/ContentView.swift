fa5bd6655fcce98808461962cb19a94d29e2fcd9
Denis Papathanasiou <denis@papathanasiou.org>
2025-12-28 16:32:21 -0500

    Initial Commit: one-password as an iOS app

---
+++ ios-one-password/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-one-password
+//
+//  Created by Denis Papathanasiou on 12/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-one-password/ios_one_passwordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_one_passwordApp.swift
+//  ios-one-password
+//
+//  Created by Denis Papathanasiou on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_one_passwordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
